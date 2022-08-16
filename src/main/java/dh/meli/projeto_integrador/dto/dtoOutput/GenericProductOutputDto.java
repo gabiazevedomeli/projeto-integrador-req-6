ab35d3426f61e1bcc7917e723629bc80254e1fa6
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-16 11:37:43 -0300

    feat: Criação de Output Dto para produto

--- src/main/java/dh/meli/projeto_integrador/dto/dtoOutput/GenericProductOutputDto.java
+++ src/main/java/dh/meli/projeto_integrador/dto/dtoOutput/GenericProductOutputDto.java
@@ -7,18 +7,39 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of GenericProductOutputDto Class
+ */
 @Getter
+/**
+ * Method Setter implemented by Lombok lib for set the private attributes of GenericProductOutputDto Class
+ */
 @Setter
+/**
+ * Method Default Constructor implemented by Lombok lib
+ */
 @NoArgsConstructor
+/**
+ * Method Constructor with all arguments implemented by Lombok lib
+ */
 @AllArgsConstructor
+/**
+ * Method builder implemented by Lombok lib
+ */
 @Builder
-public class NewProductOutputDto {
+/**
+ * Class used to create a Data Transfer Output Object to return a Product
+ * @author Gabriela Azevedo
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
+public class GenericProductOutputDto {
     private String name;
     private String type;
     private String categoryName;
     private Double price;
 
-    public NewProductOutputDto(Product product) {
+    public GenericProductOutputDto(Product product) {
         this.name = product.getName();
         this.type = product.getType();
         this.categoryName = product.getCategoryName();
