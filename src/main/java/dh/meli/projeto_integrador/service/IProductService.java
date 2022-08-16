0ed2d40251a6202af3f41267f9b8b765ab30f47f
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-15 22:07:28 -0300

    feat: Implementação dos métodos da entidade produto no controller

--- src/main/java/dh/meli/projeto_integrador/service/IProductService.java
+++ src/main/java/dh/meli/projeto_integrador/service/IProductService.java
@@ -1,11 +1,14 @@
 package dh.meli.projeto_integrador.service;
 
+import dh.meli.projeto_integrador.dto.dtoInput.NewProductDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.NewProductOutputDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.ProductStockDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.ProductOutputDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.ListProductByWarehouseDto;
 import dh.meli.projeto_integrador.model.Product;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Interface to specify service methods implemented on ProductService class.
@@ -47,4 +50,32 @@ public interface IProductService {
      * @return an output object of type ListProductByWarehouseDto
      */
     ListProductByWarehouseDto listProductByWarehouse(long productId);
+
+    /**
+     * Method implemented by ProductService that creates a one or many products at the same time
+     * @param newProductDto a List of objects of type newProductDto
+     * @return a List of type NewProductOutputDto
+     */
+    List<NewProductOutputDto> createNewProduct(List<NewProductDto> newProductDto);
+
+    /**
+     * Method implemented by ProductService that gets all products by product category name
+     * @param categoryName String
+     * @return a List of objects of type NewProductOutputDto
+     */
+    List<NewProductOutputDto> findByCategoryName(String categoryName);
+
+    /**
+     * Method implemented by ProductService that makes a partial update of a product
+     * @param productId Long product identifier
+     * @param productChanges data on format Map<Key, Value> with the info to update the product
+     * @return an updated object of type NewProductOutputDto
+     */
+    NewProductOutputDto partialUpdateOfProduct(Long productId, Map<String, ?> productChanges);
+
+    /**
+     * Method for delete a product
+     * @param productId Long product identifier
+     */
+    void deleteProduct(Long productId);
 }
